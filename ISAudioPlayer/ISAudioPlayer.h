47ec45f777fdcb83919ffc339830ada2a427852e
igor <murluck.s@gmail.com>
2017-01-14 15:19:10 +0200

    Initial Commit

---
+++ ISAudioPlayer/ISAudioPlayer.h
@@ -0,0 +1,19 @@
+//
+//  ISAudioPlayer.h
+//  ISAudioPlayer
+//
+//  Created by Igor Sokolovsky on 1/14/17.
+//  Copyright © 2017 Igor Sokolovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISAudioPlayer.
+FOUNDATION_EXPORT double ISAudioPlayerVersionNumber;
+
+//! Project version string for ISAudioPlayer.
+FOUNDATION_EXPORT const unsigned char ISAudioPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISAudioPlayer/PublicHeader.h>
+
+
